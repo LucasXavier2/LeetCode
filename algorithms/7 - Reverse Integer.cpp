39e494087eae08baca034480eb1b0ead59360586
Lucas Xavier <lucasxavier2@gmail.com>
2020-08-09 19:10:01 -0300

    Add files via upload

--- algorithms/7 - Reverse Integer.cpp
+++ algorithms/7 - Reverse Integer.cpp
@@ -7,21 +7,15 @@ public:
         if(x < 10 && x > -10) return x;
         
         long reversed_number = 0;
-        long multiplier = 1;
-        vector<int> digits;
         
         while(x != 0) {
-            digits.push_back(x % 10);
+            int digit = x % 10;
+            reversed_number = reversed_number * 10 + digit;
             x /= 10;   
         }
-        
-        for(int i = digits.size()-1; i >= 0; i--){
-            reversed_number += digits[i] * multiplier;
-            multiplier *= 10;
-        }
-        
-        //Check for overflows and underflows
+
         if(reversed_number > pow(2, 31) -1 || reversed_number < -pow(2, 31)) return 0;
         return reversed_number;
+        
     }
 };
